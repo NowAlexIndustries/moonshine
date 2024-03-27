d5ff7b15b12e04607b2ecb54660367507b12e9f4
Dari <Dariusamrein467@gmail.com>
2024-03-27 23:40:14 +0100

    add unified.js

---
+++ script/unified.js
@@ -0,0 +1,13 @@
+function reset_fokolas() {
+    document.getElementById('measuredQuantity').value = '';
+    document.getElementById('measuredTemp').value = ''; 
+    document.getElementById('fokolas-output').innerHTML = '';
+}
+
+function reset_keveres() {
+
+}
+
+function reset_higitas() {
+
+}
