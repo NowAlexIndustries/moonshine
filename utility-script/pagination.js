581cfc3b55559dc380240aa30c1751c93956e4d8
Dari <Dariusamrein467@gmail.com>
2024-03-28 15:14:28 +0100

    add placeholder style for currenty selected page nav

--- utility-script/pagination.js
+++ utility-script/pagination.js
@@ -1,13 +1,28 @@
-function paginate(pageId) {
-    // Hide all pages
-    let pages = Array.from(document.getElementsByClassName('content'));
+
+let pages = Array.from(document.getElementsByClassName('content'));
+let navItems = Array.from(document.getElementsByClassName('nav-item'));
+
+function paginate(pg) {
+    // Hide all pages 
     pages.forEach(function(page) {
         page.style.display = 'none';
     });
 
     // Show the selected page
-    let selectedPage = document.getElementById(pageId);
+    let selectedPage = document.getElementById(pg + '-page');
     if (selectedPage) {
         selectedPage.style.display = 'block';
+    }
+
+
+    // remove active-nav class from all navItems
+    navItems.forEach(function(element) {
+        element.classList.remove('active-nav');
+    });
+
+    // add active-nav class for selected page
+    let selectedNav = document.getElementById(pg + '-nav');
+    if (selectedNav) {
+        selectedNav.classList.add('active-nav');
     }
 }
